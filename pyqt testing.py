d29990595d0dc02a1692e5ff3960de39173299e4
Igor Velvetov <112966665+MrSuperParadox@users.noreply.github.com>
2025-07-11 17:25:26 +0300

    Update pyqt testing.py

--- pyqt testing.py
+++ pyqt testing.py
@@ -2,6 +2,7 @@ from pickle import GLOBAL
 
 from PyQt6.QtCore import QSize
 from PyQt6.QtWidgets import QApplication, QMainWindow, QLineEdit, QVBoxLayout, QWidget, QPushButton,  QListWidget, QPlainTextEdit, QListWidgetItem, QBoxLayout
+from cryptography.exceptions import InvalidKey
 from cryptography.fernet import Fernet
 import base64
 import hashlib
@@ -9,14 +10,9 @@ import hashlib
 import sys
 
 import os
-vault_path = r"C:\pythontest\vault"
-password_path = r"C:\pythontest\vault\.password"
-if not os.path.exists(vault_path):
-    os.makedirs(vault_path)
-if not os.path.exists(password_path):
-    with open(password_path, 'w', encoding='utf-8') as filestream:
-        pass
-
+VAULT_PATH = r"C:\pythontest\vault"
+if not os.path.exists(VAULT_PATH):
+    os.makedirs(VAULT_PATH)
 keyword = ""
 class MainWindow(QMainWindow):
     namefile = ""
@@ -26,56 +22,52 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("Diary")
         self.setFixedSize(QSize(600, 400))
 
-        self.buttonw = QPushButton("create")
-        self.buttonw.clicked.connect(self.createfile)
+        self.creatingButton = QPushButton("create")
+        self.creatingButton.clicked.connect(self.createFile)
 
-        self.deletebutton = QPushButton("delete")
-        self.deletebutton.clicked.connect(self.delete)
+        self.deleteButton = QPushButton("delete")
+        self.deleteButton.clicked.connect(self.delete)
 
-        self.r = QLineEdit()
+        self.fileCreationInput = QLineEdit()
 
         a = QListWidgetItem()
         a.setSizeHint(QLineEdit().sizeHint())
 
         self.list = QListWidget()
         self.list.addItem(a)
-        self.list.setItemWidget(a, self.r)
-        self.list.addItems(os.listdir(vault_path))
-        for i in range(self.list.count()):
-            if self.list.item(i).text() == ".password":
-                self.list.takeItem(i)
-                break
-        self.list.currentTextChanged.connect(self.current_file)
+        self.list.setItemWidget(a, self.fileCreationInput)
+        self.list.addItems(os.listdir(VAULT_PATH))
+        self.list.currentTextChanged.connect(self.currentFile)
         self.list.currentTextChanged.connect(self.read)
         self.list.setRowHidden(0, True)
 
         self.naming = QLineEdit()
         self.naming.returnPressed.connect(self.rename)
 
-        self.r.textChanged.connect(self.test)
-        self.r.returnPressed.connect(self.returnation)
+        self.fileCreationInput.textChanged.connect(self.saveName)
+        self.fileCreationInput.returnPressed.connect(self.returnation)
 
 
         self.input = QPlainTextEdit()
         self.input.textChanged.connect(self.write)
 
-        self.additionalwindow = QWidget()
+        self.additionalWindow = QWidget()
 
         layout = QVBoxLayout()
-        layoutwithnamingandinput = QVBoxLayout()
-        layoutwithbuttons = QVBoxLayout()
+        layoutWithNamingAndInput = QVBoxLayout()
+        layoutWithButtons = QVBoxLayout()
 
-        layoutwithnamingandinput.addWidget(self.naming)
-        layoutwithnamingandinput.addWidget(self.input)
+        layoutWithNamingAndInput.addWidget(self.naming)
+        layoutWithNamingAndInput.addWidget(self.input)
 
-        layoutwithbuttons.addWidget(self.buttonw)
-        layoutwithbuttons.addWidget(self.deletebutton)
-        layoutwithbuttons.addWidget(self.list)
+        layoutWithButtons.addWidget(self.creatingButton)
+        layoutWithButtons.addWidget(self.deleteButton)
+        layoutWithButtons.addWidget(self.list)
 
         layout.setDirection(QBoxLayout.Direction.LeftToRight)
         container = QWidget()
-        layout.insertLayout(0, layoutwithnamingandinput, 10)
-        layout.insertLayout(1, layoutwithbuttons, 3)
+        layout.insertLayout(0, layoutWithNamingAndInput, 10)
+        layout.insertLayout(1, layoutWithButtons, 3)
         container.setLayout(layout)
 
         self.setCentralWidget(container)
@@ -87,38 +79,38 @@ class MainWindow(QMainWindow):
             self.input.clear()
         if s != "":
             self.naming.setText(self.namefile)
-            with open(os.path.join(vault_path, self.namefile), 'rb') as filestream:
+            with open(os.path.join(VAULT_PATH, self.namefile), 'rb') as filestream:
                 g = filestream.read()
                 if len(g) == 0:
                     self.input.setPlainText("")
                     return
-                self.input.setPlainText(FernetIntegration(keyword, g, "d"))
+                self.input.setPlainText(fernetIntegration(keyword, g, "d"))
 
     def write(self):
         global keyword
-        s = self.r.text()
-        if (self.namefile or s != self.cringe(s)) and self.input.toPlainText() != "":
-            with open(os.path.join(vault_path, self.namefile), 'w') as filestream:
-                s = FernetIntegration(keyword, self.input.toPlainText(), "e")
+        s = self.fileCreationInput.text()
+        if (self.namefile or s != self.fileNameCompare(s)) and self.input.toPlainText() != "":
+            with open(os.path.join(VAULT_PATH, self.namefile), 'w') as filestream:
+                s = fernetIntegration(keyword, self.input.toPlainText(), "e")
                 filestream.write(s)
 
-    def cringe(self, u):
+    def fileNameCompare(self, u):
         for j in range(self.list.count()):
             if self.list.item(j).text() == u:
                 return self.list.item(j).text()
 
-    def current_file(self, s):
+    def currentFile(self, s):
         if s != "" or self.input.toPlainText() != "":
             self.namefile = s
 
-    def test(self, s):
+    def saveName(self, s):
         self.savename = s
 
     def rename(self):
         s = self.naming.text()
-        if self.namefile != "" and s != "" and s != self.cringe(s):
-            old_file = os.path.join(vault_path, self.namefile)
-            new_file = os.path.join(vault_path, s)
+        if self.namefile != "" and s != "" and s != self.fileNameCompare(s):
+            old_file = os.path.join(VAULT_PATH, self.namefile)
+            new_file = os.path.join(VAULT_PATH, s)
             for i in range(self.list.count()):
                 if self.list.item(i).text() == self.namefile:
                     self.naming.clear()
@@ -127,65 +119,54 @@ class MainWindow(QMainWindow):
                     self.list.addItem(s)
 
     def returnation(self):
-        s = self.r.text()
-        if s != self.cringe(s):
-            with open(os.path.join(vault_path, self.savename), 'w', encoding='utf-8') as filestream:
+        s = self.fileCreationInput.text()
+        if s != self.fileNameCompare(s):
+            with open(os.path.join(VAULT_PATH, self.savename), 'w', encoding='utf-8') as filestream:
                 self.list.addItem(self.savename)
                 self.naming.clear()
-                self.r.clear()
+                self.fileCreationInput.clear()
                 self.list.setRowHidden(0, True)
 
-    def createfile(self):
+    def createFile(self):
         self.naming.clear()
         self.input.clear()
         self.list.setRowHidden(0, False)
 
     def delete(self):
         if self.namefile:
-            os.remove(os.path.join(vault_path, self.namefile))
+            os.remove(os.path.join(VAULT_PATH, self.namefile))
             for i in range(self.list.count()):
                 if self.list.item(i).text() == self.namefile:
                     self.list.takeItem(i)
                     break
 
-class passwrodcreationwindow(QMainWindow):
+class PasswrodCreationWindow(QMainWindow):
     def __init__(self, window):
         super().__init__()
         self.window = window
-        self.passworedenter = QLineEdit()
-        self.passworedenter.setWindowTitle("Password Creation")
-        self.passworedenter.show()
-        self.passworedenter.returnPressed.connect(self.passworde)
+        self.enterPassword = QLineEdit()
+        self.enterPassword.setWindowTitle("Password Creation")
+        self.enterPassword.show()
+        self.enterPassword.returnPressed.connect(self.passworde)
 
     def passworde(self):
-        if self.passworedenter.text() != "":
-            with open(password_path, 'w', encoding='utf-8') as filestream:
-                global keyword
-                keyword = self.passworedenter.text()
-                filestream.write(FernetIntegration(keyword, keyword, "e"))
-                self.passworedenter.hide()
-                self.window.show()
+        if self.enterPassword.text() != "":
+            global keyword
+            keyword = self.enterPassword.text()
+            for j in range(self.window.list.count()):
+                if self.window.list.item(j).isHidden():
+                    continue
+                with open(os.path.join(VAULT_PATH, self.window.list.item(j).text()), "rb") as shork:
+                    a = shork.read()
+                    print(a)
+                    b = fernetIntegration(keyword, a, "d")
+                    print(b)
+                    if b is None:
+                        self.window.list.setRowHidden(j, True)
+            self.enterPassword.hide()
+            self.window.show()
 
-class PsPsWindow(QMainWindow):
-    def __init__(self, window, filestream):
-        super().__init__()
-        self.window = window
-        self.filestream = filestream
-        self.password = QLineEdit()
-        self.password.show()
-        self.password.setWindowTitle("Enter Password")
-        self.password.returnPressed.connect(self.passwordentered)
-
-    def passwordentered(self):
-        with open(password_path, 'r', encoding='utf-8') as pizdec:
-            xyq = pizdec.read()
-            if self.password.text() == FernetIntegration(self.password.text(), xyq, "d"):
-                global keyword
-                keyword = self.password.text()
-                self.password.hide()
-                self.window.show()
-
-def FernetIntegration(keypass, thing_to_crypt, mode):
+def fernetIntegration(keypass, thing_to_crypt, mode):
     key = base64.urlsafe_b64encode(hashlib.sha256(keypass.encode()).digest())
     f = Fernet(key)
     if mode == "e" :
@@ -197,15 +178,10 @@ def FernetIntegration(keypass, thing_to_crypt, mode):
             return b
         except:
             print("password error")
+            return None
 
 app = QApplication(sys.argv)
 window = MainWindow()
-
-with open(password_path, 'r', encoding='utf-8') as filestream:
-    passwrodasdadad = filestream.read()
-    if passwrodasdadad == "":
-        creatingpassword = passwrodcreationwindow(window)
-    else:
-        passwrod = PsPsWindow(window, passwrodasdadad)
+creatingPassword = PasswrodCreationWindow(window)
 
 app.exec()
